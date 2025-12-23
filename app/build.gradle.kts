61fd5ca32e8f2fe9662849e7be57a11425370dc7
Mitya Murasov <jackal.dat@yandex.ru>
2025-12-23 11:37:14 +0700

    Bump version to 0.1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "me.vripperoid.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.1.5"
+        versionCode = 8
+        versionName = "0.1.8"
         
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
